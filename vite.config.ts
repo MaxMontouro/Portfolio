baa38c40daa8e7bbe801fe630d1bd40123d040cf
Max Montouro <119320172+MaxMontouro@users.noreply.github.com>
2025-02-06 13:45:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './src/App.tsx', 
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/Portfolio/', // Remplace par le nom de ton dépôt
 });
